b25dfd25b71b9dbcb0702599797336cf540487d9
ahtamaral <artur.ht.amaral@gmail.com>
2024-10-24 19:54:05 -0300

    Fix vite not found by adding node engine and postbuild

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig( {
+export default defineConfig({
   server: {
     port: process.env.PORT || 3000,
-    host: true,
-  }
-  plugins: [react()],
-} )
+    host: true,  // Adicione uma vírgula aqui
+  },
+  plugins: [react()],  // Certifique-se de que os plugins estão fora do bloco `server`
+})
