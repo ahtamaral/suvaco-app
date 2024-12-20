35c6a6f32554a70149154d4406ef62fea3bb889b
artur_amaral <artur.amaral@mettainnovations.com.br>
2024-12-20 12:20:53 -0300

    Cria diretório de Layout, move Navbar para dentro do Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Nav from './components/Nav'
-import Header from './components/Header'
+import Header from './layouts/header/Header'
 import Sobre from './components/Home/Sobre'
 import Acervo from './components/Home/Acervo'
 import LinhaDoTempo from './components/Home/LinhaDoTempo'
 import DivinasAxilas from './components/Home/DivinasAxilas'
 import VoceComSuvaco from './components/Home/VoceComSuvaco'
-import Footer from './components/Home/Footer'
+import Footer from './layouts/footer/Footer'
 
 import PageSobre from './components/pasteSobre/PageSobre'
 import PageTimeline from './components/pasteTimeline/PageTimeline';
@@ -21,11 +21,7 @@ function App() {
 
   return (
     <div>
- 
-
-        <Nav/>
-        <Header/>
-
+          <Header/>
           <Routes>
 
             {/* Página principal */}
@@ -45,18 +41,11 @@ function App() {
             {/* Outras páginas*/}
             <Route path="/sobre" element={<PageSobre  {... sectionsPosts.sections[1]}/>} /> 
             <Route path="/timeline" element={<PageTimeline {... sectionsPosts.sections[3]}/>} />
-            
-
-            
             <Route path="/Ano2012" element={<Ano2012/>} />
-
             
           </Routes>
         
         <Footer/>
-      
-
-
     </div>
   )
 }
