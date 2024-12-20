d9916fff2c41fb3502abf684f064ceefc4399a6e
artur_amaral <artur.amaral@mettainnovations.com.br>
2024-12-20 12:35:11 -0300

    Mantém apenas 2 anos na página de timeline

--- src/components/pasteTimeline/PageTimeline.jsx
+++ src/components/pasteTimeline/PageTimeline.jsx
@@ -8,41 +8,41 @@ function PageTimeline(props){
         <div className="section-container">
             {renderTitleSubtitle(props.title,props.subtitle)}
         
-        <div className= "section-pages">
+            <div className= "section-pages">
 
-            <Link to="/Ano2012" className="img-container">
-                <img src="img/Screenshot_1.png" alt="Desfile 27 - Ano 2012" />
-                <h3>Desfile 27 - Ano 2012</h3>
-            </Link>
-            <Link to="/ano-1986" className="img-container">
-                <img src="img/Screenshot_1.png" alt="Desfile 01 - Ano 1986" />
-                <h3>Desfile 01 - Ano 1986</h3>
-            </Link>
-            <Link to="/" className="img-container">
-                <img src="" alt="" />
-                <h3>Desfile XX - Ano 20XX</h3>
-            </Link>
-            <Link to="/" className="img-container">
-                <img src="" alt="" />
-                <h3>Desfile XX - Ano 20XX</h3>
-            </Link>
-            <Link to="/" className="img-container">
-                <img src="" alt="" />
-                <h3>Desfile XX - Ano 20XX</h3>
-            </Link>
-            <Link to="/" className="img-container">
-                <img src="" alt="" />
-                <h3>Desfile XX - Ano 20XX</h3>
-            </Link>
-            <Link to="/" className="img-container">
-                <img src="" alt="" />
-                <h3>Desfile XX - Ano 20XX</h3>
-            </Link>
-            <Link to="/" className="img-container">
-                <img src="" alt="" />
-                <h3>Desfile XX - Ano 20XX</h3>
-            </Link>
-        </div>
+                <Link to="/Ano2012" className="img-container">
+                    <img src="img/Screenshot_1.png" alt="Desfile 27 - Ano 2012" />
+                    <h3>Desfile 27 - Ano 2012</h3>
+                </Link>
+                <Link to="/ano-1986" className="img-container">
+                    <img src="img/Screenshot_1.png" alt="Desfile 01 - Ano 1986" />
+                    <h3>Desfile 01 - Ano 1986</h3>
+                </Link>
+                {/* <Link to="/" className="img-container">
+                    <img src="" alt="" />
+                    <h3>Desfile XX - Ano 20XX</h3>
+                </Link>
+                <Link to="/" className="img-container">
+                    <img src="" alt="" />
+                    <h3>Desfile XX - Ano 20XX</h3>
+                </Link>
+                <Link to="/" className="img-container">
+                    <img src="" alt="" />
+                    <h3>Desfile XX - Ano 20XX</h3>
+                </Link>
+                <Link to="/" className="img-container">
+                    <img src="" alt="" />
+                    <h3>Desfile XX - Ano 20XX</h3>
+                </Link>
+                <Link to="/" className="img-container">
+                    <img src="" alt="" />
+                    <h3>Desfile XX - Ano 20XX</h3>
+                </Link>
+                <Link to="/" className="img-container">
+                    <img src="" alt="" />
+                    <h3>Desfile XX - Ano 20XX</h3>
+                </Link> */}
+            </div>
         
         </div>
     </section>
