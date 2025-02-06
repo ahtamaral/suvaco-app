2f5d67a48f2a2f77721a1b9134809c41016443d9
carloschgas <139183141+carloschgas@users.noreply.github.com>
2025-02-06 15:56:38 -0300

    refactor-carrossel
    permite a inserção de imagens no carrossel a partir dos ids

--- src/components/pasteTimeline/anos/componentes-anos/Carrossel/Carrossel.jsx
+++ src/components/pasteTimeline/anos/componentes-anos/Carrossel/Carrossel.jsx
@@ -1,28 +1,17 @@
 import React from "react";
 import './Carrossel.css'
+import DATA from '../../../../json/resultado_formatado.json'
+import { placeImageFromSheetByID } from "../../../../functions";
 
-function Carrossel(){
-
+function Carrossel({ids}){
     return (
     <div className="carrossel-extern-div">
-            <div id="warning">desenvolvimento</div>
+            <div id="warning">carrosel</div>
         <div className="carrossel">
 
-                <img className="card"src="img/1.jpg" alt="" />
-                <img className="card"src="img/2.jpg" alt="" />
-                <img className="card"src="img/3.jpg" alt="" />
-                <img className="card"src="img/4.jpg" alt="" />
-                <img className="card"src="img/5.jpg" alt="" />
-                <img className="card"src="img/6.jpg" alt="" />
-                <img className="card"src="img/7.jpg" alt="" />
-                <img className="card"src="img/8.jpg" alt="" />
-                <img className="card"src="img/9.jpg" alt="" />
-                <img className="card"src="img/10.jpg" alt="" />
-                <img className="card"src="img/11.jpg" alt="" />
-                <img className="card"src="img/12.jpg" alt="" />
-                <img className="card"src="img/13.jpg" alt="" />
-                <img className="card"src="img/14.jpg" alt="" />
-                <img className="card"src="img/15.jpg" alt="" />
+            {ids.map((id) => placeImageFromSheetByID(id,DATA))}
+
+
 
 
             </div>
