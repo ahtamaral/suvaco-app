59941ef432b07fb61e6ee42c8202fdd5b80af2d1
carloschgas <139183141+carloschgas@users.noreply.github.com>
2025-02-11 21:03:03 -0300

    muitas features

--- src/components/pasteTimeline/anos/1986/Ano1986.jsx
+++ src/components/pasteTimeline/anos/1986/Ano1986.jsx
@@ -7,23 +7,27 @@ import PlaceImageFromSheetByID from "../componentes-anos/imgByID/PlaceImageFromS
 import Carrossel from "../componentes-anos/Carrossel/Carrossel";
 
 function Ano1986() {
-    //cada item do array representa a visibilidade de uma divisória
-    const [divisoriasVisibilidade, setDivisoriasVisibilidade] = useState([false, false,false, false, false]);
-    const [indiceAtual, setIndiceAtual] = useState(0);
     
     const artistas_SAMBA = [
         { funcao: "AUTOR", nome: "João Avelleira", imagem: "img/96-modified.png" },
         { funcao: "INTÉRPRETE", nome: "Sonia Matos", imagem: "img/96-modified.png" },
-        { funcao: "AUTOR", nome: "João Avelleira", imagem: "img/96-modified.png" },
-        { funcao: "AUTOR", nome: "2", imagem: "img/96-modified.png" },
-        { funcao: "INTÉRPRETE", nome: "2", imagem: "img/96-modified.png" },
-        { funcao: "AUTOR", nome: "2", imagem: "img/96-modified.png" }
+        { funcao: "AUTOR", nome: "João Avelleira", imagem: "img/96-modified.png" }
     ];
 
     const artistas_ARTE = [
 
         { funcao: "INTÉRPRETE", nome: "Sonia Matos", imagem: "img/96-modified.png" },
     ]
+
+
+    const [estadoVisibilidadeDivisorias, setEstadoVisibilidadeDivisorias ] = useState([true,true,true,true,true]);
+
+    const toggleDivisoria = (index) => {
+        setEstadoVisibilidadeDivisorias(prevState => 
+            prevState.map((visivel, i) => (i === index ? !visivel : visivel))
+        );
+    };
+    
 
     return (
         <section>
@@ -36,47 +40,61 @@ function Ano1986() {
                 <div className="acesso-rapido">
                     <span id="acesso">Acesso Rápido</span>
                     <div className="box">
-                        <ul className="box-links">
-                            <li>O Contexto Histórico</li>
-                            <li>O Samba</li>
-                            <li>O Processo Criativo</li>
-                            <li>O Suvaco na Mídia</li>
-                        </ul>
+                    <ul className="box-links">
+                        <li><a href="#contexto-historico">O Contexto Histórico</a></li>
+                        <li><a href="#o-samba">O Samba</a></li>
+                        <li><a href="#processo-criativo">O Processo Criativo</a></li>
+                        <li><a href="#suvaco-midia">O Suvaco na Mídia</a></li>
+                        <li><a href="#o-desfile">O Desfile</a></li>
+
+                    </ul>
+
                     </div>
 
                 </div>
 
                 
-                <div className="div1"> {/*DIVISÓRIA 1*/}
+                <div id="contexto-historico" className="div0"> {/*DIVISÓRIA 0*/}
                     <div className="section-title-subtitle"> {/*TÍTULO*/}
                                     
                         <h2 id="titulo-ancora">Contexto Histórico</h2>
-                        <button id="subtitle">FECHAR</button>
+                        <button id="subtitle" onClick={() => toggleDivisoria(0)}>
+                            {estadoVisibilidadeDivisorias[0] ? "FECHAR" : "ABRIR"}
+                        </button>
                         <div className="subtitle-line"></div>
                                                                         {/*FIM TÍTULO*/}
                     </div>
+
+                    
                                     
+                    {estadoVisibilidadeDivisorias[0] && (
                         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, dignissimos nemo, sed distinctio, impedit recusandae qui cupiditate magnam deserunt quasi natus! Aut ducimus incidunt aperiam ea at voluptas distinctio recusandae.
                                     Est placeat tempora voluptates deleniti, obcaecati earum illum possimus hic dignissimos vel. Eius excepturi natus beatae ex quibusdam ipsa eum, aspernatur quisquam deleniti iste accusantium minima adipisci minus sint cum.
                                     Nulla ad iure, assumenda, praesentium molestiae vel aliquam eum neque, nesciunt accusamus deleniti autem reprehenderit qui eaque consectetur minima eveniet optio. Commodi corrupti aliquid quas laborum excepturi molestias dignissimos pariatur.
                                     Eum, natus omnis enim reiciendis, totam maiores ab, quae officiis id repellendus fuga quisquam! Possimus dolore repellat repellendus consectetur placeat. Fugit porro quaerat, adipisci cumque tempore libero. Impedit, nisi officiis.
                                     Sapiente cum voluptatibus nihil ex. Hic porro quibusdam aspernatur, ipsam sit autem ducimus provident dolor ipsum nihil iusto magnam numquam unde cum tenetur! Iste qui sed dolor, temporibus alias excepturi?
                         </p>
+                    )}
 
                                     
-                </div> {/*FIM DIVISÓRIA 1*/}
+                </div> {/*FIM DIVISÓRIA 0*/}
 
                 
-                <div className="div2"> {/*DIVISÓRIA 2*/}
+                <div id="o-samba" className="div1"> {/*DIVISÓRIA 1*/}
                     <div className="section-title-subtitle"> {/*TÍTULO*/}
                                     
                         <h2 id="titulo-ancora">O Samba</h2>
-                        <button id="subtitle">FECHAR</button>
+                        <button id="subtitle" onClick={() => toggleDivisoria(1)}>
+                            {estadoVisibilidadeDivisorias[1] ? "FECHAR" : "ABRIR"}
+                        </button>
                     <div className="subtitle-line"></div>
-                                                                        {/*FIM TÍTULO*/}
                     </div>
+                                                                        {/*FIM TÍTULO*/}
                 
-                    <div className="subdiv2-1"> {/*SUBDIVISAO -  LETRA*/}
+                    {estadoVisibilidadeDivisorias[1] && ( 
+                        <>
+                        <div className="subdiv1-1"> 
+
                         
                         <h3 className="subtitle-subdiv">Divinas Axilas</h3>
                         <div className="letra-side-by-side">
@@ -105,13 +123,13 @@ function Ano1986() {
                         </div>
                     </div>
                     
-                    <div className="subdiv2-2"> {/* SUBDIVISAO 2 - ESCUTE SAMBA */}
+                    <div className="subdiv1-2"> {/* SUBDIVISAO 2 - ESCUTE SAMBA */}
                         <h3 className="subtitle-subdiv" style={{marginBottom:"75px"}}>Escute o Samba</h3>
                         <Player audioSrc={"songs/2012 Suvaco Samba Palmas pro Suvaco.mp3"}></Player>
 
                     </div>
 
-                    <div className="subdiv2-3" style={{marginTop:"50px"}}> {/* SUBDIVISAO 3 - POR DE TRAS LETRA */}
+                    <div className="subdiv1-3" style={{marginTop:"50px"}}> {/* SUBDIVISAO 3 - POR DE TRAS LETRA */}
                         <h3 className="subtitle-subdiv">Por trás da letra</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maxime, natus doloribus voluptatum accusantium veritatis non voluptas eveniet nihil minima consequuntur aliquid inventore aspernatur, perferendis, quis officiis nisi magni molestias?
                         Dolorum unde ipsam illo natus, laudantium dignissimos nulla blanditiis fugiat distinctio in. Tempora similique molestiae quisquam placeat distinctio voluptatem quas possimus ex dolores nisi beatae, ullam culpa cum debitis neque?
@@ -120,24 +138,28 @@ function Ano1986() {
                         Voluptates, ipsum. Illum deleniti molestias quo magnam consectetur animi modi amet nulla, praesentium et voluptatibus consequatur, quam non numquam blanditiis, quisquam sapiente excepturi debitis tenetur! Inventore perspiciatis ipsam placeat nisi?</p>
 
                     </div>
-                    <div className="subdiv2-4"> 
+                    <div className="subdiv1-4"> 
                         <h3 className="subtitle-subdiv">Autores e Intérpretes</h3>
                         <Carrossel artistas={artistas_SAMBA} />
                             </div>
+                        </>
+                        )}
 
                                     
-                </div> {/*FIM DIVISÓRIA 2*/}
+                </div> {/*FIM DIVISÓRIA 1*/}
                 
-                <div className="div3"> {/*DIVISÓRIA 3*/}
+                <div id="processo-criativo" className="div2"> {/*DIVISÓRIA 2*/}
                     <div className="section-title-subtitle"> {/*TÍTULO*/}
                                     
                         <h2 id="titulo-ancora">Artes do Desfile</h2>
-                        <button id="subtitle">FECHAR</button>
+                        <button id="subtitle" onClick={() => toggleDivisoria(2)}>
+                            {estadoVisibilidadeDivisorias[2] ? "FECHAR" : "ABRIR"}
+                        </button>
                         <div className="subtitle-line"></div>
                                                                         {/*FIM TÍTULO*/}
                     </div>
                     
-                    <div className="subdiv3-1"> {/* SUBDIVISAO 2 - ESCUTE SAMBA */}
+                    {estadoVisibilidadeDivisorias[2] && (<><div className="subdiv3-1"> {/* SUBDIVISAO 2 - ESCUTE SAMBA */}
                         <h3 className="subtitle-subdiv" style={{marginBottom:"75px"}}>Camiseta</h3>
                         <PlaceImageFromSheetByID id={2} sheet={DATA} objClass={"center"}></PlaceImageFromSheetByID>
                         <p style={{textAlign:"center"}}>Design da camiseta de 1986</p>
@@ -146,51 +168,54 @@ function Ano1986() {
                     <div className="subdiv3-2"> 
                         <h3 className="subtitle-subdiv">Artistas</h3>
                         <Carrossel artistas={artistas_ARTE} />
-                        </div>
+                        </div></>)}
 
                                     
-                </div> {/*FIM DIVISÓRIA 3*/}
+                </div> {/*FIM DIVISÓRIA 2*/}
 
-                <div className="div4" style={{marginTop: "25px"}}> {/*DIVISÓRIA 4*/}
+                <div id="suvaco-midia" className="div3" style={{marginTop: "25px"}}> {/*DIVISÓRIA 3*/}
                     <div className="section-title-subtitle"> {/*TÍTULO*/}
                                     
                         <h2 id="titulo-ancora">O Suvaco na Mídia</h2>
-                        <button id="subtitle">FECHAR</button>
+                        <button id="subtitle" onClick={() => toggleDivisoria(3)}>
+                            {estadoVisibilidadeDivisorias[3] ? "FECHAR" : "ABRIR"}
+                        </button>
                         <div className="subtitle-line"></div>
                                                                         {/*FIM TÍTULO*/}
                     </div>
-                    
-                    <div className="flex-midia">
-                    <i className="ri-arrow-left-line arrow"></i>
-                    <div className="sistema-colunas">
-                        <PlaceImageFromSheetByID id={18} sheet={DATA} objClass={"img-coluna"} ObjID={"nasce-suvaco"}></PlaceImageFromSheetByID>
-                        <PlaceImageFromSheetByID id={15} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
-                        <PlaceImageFromSheetByID id={17} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
-                        <PlaceImageFromSheetByID id={14} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
-                        <PlaceImageFromSheetByID id={16} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
-                        <PlaceImageFromSheetByID id={19} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
-                        <PlaceImageFromSheetByID id={13} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
+                    {estadoVisibilidadeDivisorias[3] && (
+                        <>
+                        <div className="flex-midia">
+                        <div className="sistema-colunas">
+                            <PlaceImageFromSheetByID id={18} sheet={DATA} objClass={"img-coluna"} ObjID={"nasce-suvaco"}></PlaceImageFromSheetByID>
+                            <PlaceImageFromSheetByID id={15} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
+                            <PlaceImageFromSheetByID id={17} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
+                            <PlaceImageFromSheetByID id={14} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
+                            <PlaceImageFromSheetByID id={16} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
+                            <PlaceImageFromSheetByID id={19} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
+                            <PlaceImageFromSheetByID id={13} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
+                        </div>
                     </div>
-                    <i className="ri-arrow-right-line arrow"></i>
-                    </div>
+                        </>)}
                     
 
 
                                     
-                </div> {/*FIM DIVISÓRIA 4*/}
+                </div> {/*FIM DIVISÓRIA 3*/}
                 
                 
-                <div className="div5" style={{marginTop: "25px"}}> {/*DIVISÓRIA 5*/}
+                <div id="o-desfile" className="div4" style={{marginTop: "25px"}}> {/*DIVISÓRIA 4*/}
                     <div className="section-title-subtitle"> {/*TÍTULO*/}
                                     
                         <h2 id="titulo-ancora">O Desfile</h2>
-                        <button id="subtitle">FECHAR</button>
+                        <button id="subtitle" onClick={() => toggleDivisoria(4)}>
+                            {estadoVisibilidadeDivisorias[4] ? "FECHAR" : "ABRIR"}
+                        </button>
                         <div className="subtitle-line"></div>
                                                                         {/*FIM TÍTULO*/}
                     </div>
-                    
+                    {estadoVisibilidadeDivisorias[4] && (<>
                     <div className="flex-midia">
-                    <i className="ri-arrow-left-line arrow"></i>
                     <div className="sistema-colunas">
                         <PlaceImageFromSheetByID id={6} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
                         <PlaceImageFromSheetByID id={10} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
@@ -199,11 +224,10 @@ function Ano1986() {
                         <PlaceImageFromSheetByID id={7} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
                         <PlaceImageFromSheetByID id={8} sheet={DATA} objClass={"img-coluna"}></PlaceImageFromSheetByID>
                     </div>
-                    <i className="ri-arrow-right-line arrow"></i>
                     </div>
                     
 
-
+                    </>)}
                                     
                 </div> {/*FIM DIVISÓRIA 5*/}
 
