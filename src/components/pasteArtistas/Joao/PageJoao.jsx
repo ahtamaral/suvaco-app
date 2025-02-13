7a3678fc8304c1ce894877cbe4c909e7e1da8b13
Julia Vilela <juliavsb@dcc.ufrj.br>
2025-02-13 11:08:30 -0300

    alterei a primeira sessao da pagina para o design do figma

--- src/components/pasteArtistas/Joao/PageJoao.jsx
+++ src/components/pasteArtistas/Joao/PageJoao.jsx
@@ -3,33 +3,45 @@ import { placeImageFromSheetByID, renderTitleSubtitle } from "../../functions";
 import './PageJoao.css'
 import DATA from '../../json/resultado_formatado.json'
 import PlaceImageFromSheetByID from "../../pasteTimeline/anos/componentes-anos/imgByID/PlaceImageFromSheetByID";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 
 function PageJoao(){
+
+    const config = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
 
     return (
         <section>
             <div className="section-container">
                 {renderTitleSubtitle("João Avelleira", null)}
 
-                <div className="primeiro-paragrafo">
-                <img id="perfil-joao" src="img/96.jpg" alt="" />
+                <div className="section-1">
+                <img id="perfil-artista" src="img/96.jpg" alt="" />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium perspiciatis, eum exercitationem possimus harum officia commodi nesciunt velit totam voluptatibus eveniet enim quos. Sit possimus porro odit totam vel sequi.
                 Iusto, doloribus voluptatibus! Totam vitae odit sed provident esse error aliquid a, vero doloremque illo expedita veniam eaque quisquam ea. Modi magni fuga beatae. Doloribus iure delectus sapiente ad tempora!
                 Esse facere, dicta repellat quam ea nulla accusamus perspiciatis fugiat obcaecati, ad ab placeat. Dolore tempora maiores eveniet voluptas quidem facere. Ducimus officiis quasi illum maiores expedita? Neque, temporibus asperiores?
-                Velit earum quos ipsam inventore! Illo, laboriosam. Tempore labore quisquam ipsa dicta iste quia, aspernatur aperiam illum fugiat aliquam voluptates laboriosam deserunt eligendi repudiandae quo obcaecati, deleniti eos. Quod, error!
-                Expedita non voluptas fugiat molestias inventore cupiditate ab nesciunt accusamus nisi natus, cum eligendi. Repellat voluptate, incidunt architecto praesentium ipsa, accusantium rem corporis eos libero minima neque! Numquam, facilis repellat</p>
+                Velit earum quos ipsam inventore!</p>
                 </div>
                 
+                <div className="Obras">
+                    {renderTitleSubtitle("Obras", null)}
+                    <Slider {...config}>
+                        
+                    </Slider>
 
-                <h3 className="titulo-artista">Trabalhos Realizados</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae harum, velit mollitia suscipit praesentium totam atque cupiditate, iusto excepturi vero tempora et quos. Accusamus ipsum quisquam numquam placeat. Sed, corrupti!
-                Mollitia tempore asperiores quaerat impedit laboriosam, aliquam cum porro quis itaque soluta? Velit, sint! Tempore modi nisi numquam voluptates similique, officiis inventore eligendi eius deleniti obcaecati vitae accusamus dolorum totam.
-                Maxime possimus, rerum, architecto minima laudantium quos nesciunt nemo obcaecati deleniti, molestias at alias. Neque sapiente, aperiam, similique id eaque nesciunt debitis doloribus veritatis doloremque eos, iusto quae possimus molestias.
-                Nostrum sunt facilis ullam temporibus hic omnis, illo illum, quidem delectus nobis modi odio quasi. Ipsam eligendi dicta veritatis enim iure. Cum quas aliquid veritatis sapiente perferendis voluptate laborum non.
-                Dicta optio doloremque delectus repudiandae. Repellat, velit cupiditate minus id perferendis, molestias, hic rerum porro deserunt necessitatibus atque? Dolor vero nobis illo consequuntur veritatis sit molestiae optio reiciendis ullam amet!</p>
-                
+                </div>
 
+            <div>
+
+            </div>
                 <span id="aviso">A ideia aqui é colocar as letras dos sambas/camisetas do artista</span>
 
                 <h3 className="titulo-artista">Galeria do Artista</h3>
