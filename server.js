7fcfb482eb98ac3d14ff3f332f7724083b880d89
vitor.motta1 <vitor.motta@netlex.com.br>
2025-02-14 19:47:56 -0300

    Adjusted PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Serve os arquivos estáticos da build do Vite
 app.use(express.static(path.join(__dirname, 'dist')));
