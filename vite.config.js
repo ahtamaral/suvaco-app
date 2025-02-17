98df2d5dc78f557e708bdba03ca91c7eeca19880
vitor.motta1 <vitor.motta@netlex.com.br>
2025-02-17 10:02:57 -0300

    fix-domains

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,13 @@ export default defineConfig({
       usePolling: true,
     },
     cors: {
-      origin: ["https://www.suvacodocristo.com", "https://www.suvacodocristo.com.br"],
+      origin: ["https://www.suvacodocristo.com", "https://www.suvacodocristo.com.br", "museu-virtual-suvaco-7db1ebfa088f.herokuapp.com"],
       credentials: true,
     },
     clientPort: 443,
   },
   preview: {
-    allowedHosts: ["www.suvacodocristo.com", "www.suvacodocristo.com.br"]
+    allowedHosts: ["www.suvacodocristo.com", "www.suvacodocristo.com.br", "museu-virtual-suvaco-7db1ebfa088f.herokuapp.com"]
   },
   plugins: [react()],
 })
