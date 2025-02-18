8a2db51c6dd0fb695eb771e042eee1d10dfae98b
Julia Vilela <juliavsb@dcc.ufrj.br>
2025-02-18 13:21:50 -0300

    removi elementos que não serão utilizados

--- src/components/pasteArtistas/Joao/PageJoao.jsx
+++ src/components/pasteArtistas/Joao/PageJoao.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import { placeImageFromSheetByID, renderTitleSubtitle } from "../../functions";
 import './PageJoao.css'
+import { Link } from "react-router-dom";
 import DATA from '../../json/resultado_formatado.json'
 import PlaceImageFromSheetByID from "../../pasteTimeline/anos/componentes-anos/imgByID/PlaceImageFromSheetByID";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import Carrossel from "../../pasteTimeline/anos/componentes-anos/Carrossel/Carrossel";
 
 
 function PageJoao(){
 
-    const config = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
+    const artista = [
+
+    ]
 
     return (
         <section>
@@ -33,31 +28,17 @@ function PageJoao(){
                 
                 <div className="Obras">
                     {renderTitleSubtitle("Obras", null)}
-                    <Slider {...config}>
-                        
-                    </Slider>
-
-                </div>
-
-            <div>
-
-            </div>
-                <span id="aviso">A ideia aqui é colocar as letras dos sambas/camisetas do artista</span>
-
-                <h3 className="titulo-artista">Galeria do Artista</h3>
-                <span id="aviso">A ideia aqui é colocar as fotos do artista no bloco / Fotos da Pessoa</span>
-                <div className="acervo">
-                <PlaceImageFromSheetByID id={7} sheet={DATA}></PlaceImageFromSheetByID>
-                <PlaceImageFromSheetByID id={8} sheet={DATA}></PlaceImageFromSheetByID>
-                <PlaceImageFromSheetByID id={9} sheet={DATA}></PlaceImageFromSheetByID>
-                <PlaceImageFromSheetByID id={10} sheet={DATA}></PlaceImageFromSheetByID>
-                <PlaceImageFromSheetByID id={12} sheet={DATA}></PlaceImageFromSheetByID>
-                <PlaceImageFromSheetByID id={8} sheet={DATA}></PlaceImageFromSheetByID>
-                </div>
-
                 
+                </div>
 
+
+                <div className="footer-section">
+                    <Link id="link-txt" to="/">VOLTAR</Link>
+                    <div className="line"></div>
+                </div>
             </div>
+
+            
         </section>
     )
 }
