6527ab3d2a168bfe1df4b336fe4a61a40e777463
tiagomsIC <tiagoms@ic.ufrj.br>
2025-02-21 01:21:41 -0300

    videoHomePage

--- src/components/Home/Video.jsx
+++ src/components/Home/Video.jsx
@@ -6,7 +6,7 @@ function Video(){
     <div className="video-section">
       <div className="gradient-side left"></div>
         <video autoPlay loop muted className="background-video">
-            <source src="/video/videoHome.mp4" type="video/mp4" />
+            <source src="/video/videoHomePage.mp4" type="video/mp4" />
             Seu navegador não suporta a reprodução de vídeos.
         </video>
     </div>
