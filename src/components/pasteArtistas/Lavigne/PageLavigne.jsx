8d261d30de1d52b70de83c232d60b7a25499b2ef
Julia Vilela <juliavsb@dcc.ufrj.br>
2025-02-21 13:15:48 -0300

    teste de conflito

--- src/components/pasteArtistas/Lavigne/PageLavigne.jsx
+++ src/components/pasteArtistas/Lavigne/PageLavigne.jsx
@@ -11,7 +11,9 @@ function PageLavigne(){
                 {renderTitleSubtitle("José Lavigne", null)}
                 <div className="primeiro-paragrafo">
                 <img id="perfil-artista" src="img/joseLavigne-doc.jpg" alt="" />
-                <p>O bloco Força Jovem do Horto, localizado no bairro de mesmo nome próximo ao Jardim Botânico, foi um parceiro importante durante os três primeiros anos do Suvaco. Os ensaios do Suvaco aconteciam na quadra do Força Jovem e a base da bateria era composta pelo pessoal do Força Jovem. Roberto de Assis, também conhecido como Pirulito, era o  presidente do bloco Força Jovem na época, e  participou da construção do primeiro samba do Suvaco  (‘’Divinas Axilas’’), como revela no documentário 20 anos de Suvaco o sociólogo Arnaldo Chaim: "Em cinco minutos, ele transformou aquele monstro em samba". A relação entre os dois blocos terminaria após três anos com a mudança do Suvaco para o Clube Condomínio.
+                <p>Nascido no Rio de Janeiro, José Lavigne de Lemos Filho enxergou no teatro do Liceu Franco Brasileiro um escape para a rigidez de sua educação. De lá, entrou para o Tablado, escola de atuação fundada em 1951 pela atriz, escritora e diretora Maria Clara Machado. Após abandonar a universidade em que cursava direito para dedicar-se exclusivamente ao mundo das artes, Lavigne ingressou no grupo “Asdrúbal Trouxe o Trombone”. Posteriormente fundou, junto com outros artistas,  o “Manhas e Manias”, focado em apresentações circenses, onde passou a desenvolver suas primeiras experiências como diretor. Ao ingressar na Globo, em 1986, Lavigne passou a dirigir programas  humorísticos de grande sucesso da televisão brasileira, assim como programas jornalísticos. Em 1985, na época da criação do Suvaco do Cristo, era um dos moradores do prédio 741 da Rua Maria Angélica, onde nossa odisseia começou!
+
+
 
                 </p>
                 </div>
